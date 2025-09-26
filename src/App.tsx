904f0c18d32faa87aa9f2ff98528d7e99b644995
nagham <naghamelgreeny@gmail.com>
2025-09-26 17:40:32 +0300

    Initialize project with React + Vite + Tailwind + TS + Sass

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+console.log('app is ready')
+  return (
+    <>
+    <h2>TO DO APP</h2>
+     <p>initial state of todo app ..</p>
+    </>
+  )
+}
+
+export default App
