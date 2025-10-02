19041d0b8dc4b3be3b12e429f662742c7975e936
nagham <naghamelgreeny@gmail.com>
2025-10-02 16:31:14 +0300

    fix: tasklist overflow

--- src/components/Sections/ToDoList.tsx
+++ src/components/Sections/ToDoList.tsx
@@ -4,7 +4,6 @@ import type { Task } from "../../features/todos/models/Task";
 import TaskInput from "../../features/todos/components/TaskInput";
 import TaskList from "../../features/todos/components/TaskList";
 
-
 const taskManager = new TaskManager();
 
 const ToDoList = () => {
@@ -13,8 +12,8 @@ const ToDoList = () => {
   const refresh = () => setTasks([...taskManager.getTasks()]);
 
   const handleAddTask = (title: string) => {
-      taskManager.addTask(title);
-      console.log(taskManager.getTasks());
+    taskManager.addTask(title);
+    console.log(taskManager.getTasks());
     refresh();
   };
 
@@ -34,14 +33,16 @@ const ToDoList = () => {
   };
 
   return (
-    <section className="bg-green-400/20 rounded-3xl p-4 shadow-md w-full max-w-2xl mx-auto min-h-full">
+    <section className="bg-green-400/20 rounded-3xl p-4 shadow-md w-full max-w-2xl mx-auto min-h-full flex flex-col">
       <TaskInput onAdd={handleAddTask} />
-      <TaskList
-        tasks={tasks}
-        onToggle={handleToggleTask}
-        onDelete={handleDeleteTask}
-        onUpdate={handleUpdateTask}
-      />
+      <div className="flex-1 overflow-y-auto mt-1 pr-1 custom-scroll">
+        <TaskList
+          tasks={tasks}
+          onToggle={handleToggleTask}
+          onDelete={handleDeleteTask}
+          onUpdate={handleUpdateTask}
+        />
+      </div>
     </section>
   );
 };
